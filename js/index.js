a05d91f338bda6daec2cb8ec6525a7aa05ad382d
Saidbek <106152417+Saidbek2409@users.noreply.github.com>
2023-04-17 18:25:48 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// let doc = document
+// let url1 = 'https://www.1zoom.ru/big2/541/255095-Sepik.jpg'
+// let img = doc.querySelector('img')
+// for(let item of img){
+//     item.style.width  = '100px'
+// }
+// img.setAttribute('src', url1)
+
+// img.alt = 'Картинка кода'
+
+
+// console.log(img.getAttribute('src'));
+
+
